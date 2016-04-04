2120ec1ad1c9f0b0f352d96f31f2a353e2b3c8bb
lars <lars_la@live.com>
2016-04-04 15:37:37 +0200

    Added music and sound, yey

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,15 +10,29 @@ app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
     $scope.condomSwitch = true;
     $scope.message = "Spillet starter, lykke til!";
 
+    var waterSound = new Audio("../sound/water.mp3");
+    var condomSound = new Audio("../sound/condom.mp3");
+    var diceSound = new Audio("../sound/dice.mp3");
+    var foodSound = new Audio("../sound/food.mp3");
+    var sexSound = new Audio("../sound/sex.mp3");
+    var smokeSound = new Audio("../sound/smoke.mp3");
+    var sneezeSound = new Audio("../sound/sneeze.mp3");
+    var deadSound = new Audio("../sound/dead.mp3");
+    var lowHealthSound = new Audio("../sound/lowHealth.mp3");
+    var idleSound = new Audio("../sound/idle.mp3");
+
+
     //--------------------Functions to perform actions--------------------------
     $scope.drinkWater = function() {
         $scope.thirst = 0;
         $scope.message = "Du drakk vann"
+        waterSound.play();
     }
     $scope.eatHealthyFood = function() {
         $scope.hunger = 0;
         checkHealth(10, '+');
         $scope.message = "Du spiste mat"
+        foodSound.play();
     }
 
     $scope.party = function() {
@@ -27,12 +41,14 @@ app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
         $scope.message = "Du hadde en fest. Du ble gladere, men mistet helse."
     }
     $scope.rollDice = function() {
+        diceSound.play();
         return(Math.floor((Math.random() * 6) + 1));
     }
     $scope.useCondom = function() {
         if ($scope.condomSwitch == true) {
             $scope.message = "Du tok på deg kondom"
             $scope.condomSwitch = false;
+            condomSound.play();
         }
         else if ($scope.condomSwitch == false) {
             $scope.message = "Kondomen er brukt"
@@ -43,24 +59,31 @@ app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
         checkHealth(10, '-');
         checkStress(5)
         $scope.message = "Du røykte. Du ble litt mindre stresset, men mistet litt helse"
+        smokeSound.play();
     }
     $scope.dropContamination = function() {
         checkHealth(30, '-');
         $scope.message = "Du har blitt smittet av tuberkolose!"
+        sneezeSound.play();
     }
     $scope.haveSex = function() {
         if (!$scope.condomSwitch) {
             $scope.message = "Du hadde trygg sex med kondom, bra!"
             $scope.condomSwitch = true;
+            checkStress(30)
+            sexSound.play();
         }
         else if ($scope.condomSwitch) {
             var n = $scope.rollDice()
-            console.log(n)
             if ((n === 5) || (n === 6)) {
                 $scope.message = "Du hadde utrygg sex, men hadde flaks."
+                checkStress(30);
+                sexSound.play();
             }
             else {
                 $scope.getStd()
+                checkStress(30);
+                sexSound.play();
             }
         }
     }
@@ -129,6 +152,7 @@ app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
     }, 500)
 
     $interval(function() {
+        var tempYear = $scope.year;
         if ($scope.hunger >= 90) {
             $scope.health -= 1;
             $scope.message = "Du sulter og mister helse!"
@@ -141,8 +165,20 @@ app.controller('MainCtrl', ['$scope', '$interval', function($scope, $interval) {
             $scope.health -= 1;
             $scope.message = "Du er veldig ulykkelig og mister helse!"
          }
+        if ($scope.health <= 30) {
+            lowHealthSound.play();
+            idleSound.pause();
+        }
         if ($scope.health <= 0) {
             $scope.health = 0;
+            idleSound.pause();
+            lowHealthSound.pause();
+            deadSound.play();
          }
+        if ($scope.health >= 31) {
+            idleSound.play();
+            lowHealthSound.pause();
+        }
     }, 1000)
+
 }]);
